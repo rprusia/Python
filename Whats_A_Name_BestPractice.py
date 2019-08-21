87a5a2c1758076f7fd3c995cce7b458eaaaeb0fe
Ray Prusia <43785857+rprusia@users.noreply.github.com>
2019-08-21 10:25:04 -0400

    Add files via upload

--- Whats_A_Name_BestPractice.py
+++ Whats_A_Name_BestPractice.py
@@ -1,3 +1,9 @@
+# What is in a name
+# or rather, what's a name in? For us, a particular string is where
+# we are looking for a name.
+# Test weather or not the string contains all the letter which spell
+# a given name in order
+
 def name_in_str(str, name):
     it = iter (str.lower())
     return all(c in it for c in name.lower())
